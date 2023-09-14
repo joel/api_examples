f55049486ac0419868b288f4c8c54a5ff0f3c3cc
Joel Azemar <joel.azemar@gmail.com>
2023-09-14 15:21:21 +0200

    Add VersionPatcher

---
+++ app/api_patches/api/v1/response/response_patcher.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "api/v1/response/users/collapse_name"
+require "api/v1/response/users/rename_name"
+
+module Api
+  module V1
+    module Response
+      class ResponsePatcher
+        attr_reader :data, :api_version, :endpoint
+
+        VERSIONS = %w[
+          2023-09-15
+          2023-09-01
+          2023-08-15
+          2023-08-01
+        ].freeze
+
+        STRATEGIES = {
+          users: {
+            "2023-09-01" => Users::CollapseName,
+            "2023-08-01" => Users::RenameName
+            # Add other strategies for users endpoint.
+          }
+          # Add other endpoints here.
+        }.freeze
+
+        def initialize(data, api_version, endpoint)
+          @data        = data
+          @api_version = api_version
+          @endpoint    = endpoint
+        end
+
+        def apply
+          requested_version_index = VERSIONS.index(api_version)
+          patches_to_apply        = VERSIONS[0..requested_version_index]
+
+          patches_to_apply.each do |version|
+            strategy = STRATEGIES[endpoint][version]
+            @data = strategy.apply(@data) if strategy
+          end
+
+          @data
+        end
+      end
+    end
+  end
+end
---
+++ app/api_patches/api/v1/response/users/rename_name.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Response
+      module Users
+        class RenameName
+          def self.apply(data)
+            data.each do |user|
+              user["fullname"] = user["name"]
+              user.delete("name")
+            end
+          end
+        end
+      end
+    end
+  end
+end
